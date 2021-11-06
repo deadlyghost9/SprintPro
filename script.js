7c5bfaa387bb718867cf41f11680bce172899e2a
Ghost09 <41258872+deadlyghost9@users.noreply.github.com>
2021-11-06 16:09:11 +0530

    Adding Pages

---
+++ script.js
@@ -0,0 +1,26 @@
+var usernamereg="";
+var passwordreg="";
+function passwordValidation(txt) {
+    //alert("ASDNKJ");
+    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,12}$/;
+    if (txt.value.match(decimal)) {
+        return true;
+    }
+    else {
+        alert("Please insert valid password");
+        return false;
+    }
+}
+function registerStore()
+{
+    usernamereg = document.registration.user-name.value;
+    passwordreg = document.registration.password.value;
+
+}
+function authentication()
+{
+    var usernamel = document.login.userName.value;
+    var passwordl = document.login.password.value;
+    alert(usernamel+" "+passwordl);
+
+}
